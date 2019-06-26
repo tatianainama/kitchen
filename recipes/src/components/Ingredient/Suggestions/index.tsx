df61dae5e629fdd04b19d81ead6e3132b5621b48
Tatiana Inama <tatii.oxo@gmail.com>
2019-06-26 15:31:11 +0200

    Replace suggestions icon

--- recipes/src/components/Ingredient/Suggestions/index.tsx
+++ recipes/src/components/Ingredient/Suggestions/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Suggestion } from 'types/recipes';
 import Icon from 'components/Icon';
 import Button from 'components/Button';
+
 import './styles.scss';
 
 type SuggestionsProps = {
@@ -12,7 +13,7 @@ type SuggestionsProps = {
 const Suggestions = ({ suggestions, onSelect }: SuggestionsProps) => {
   return suggestions && suggestions.length > 1 ? (
     <div className='cbk-suggestions'>
-      <Icon material icon={'new_releases'} />
+      <Icon material icon={'wb_incandescent'} />
       {
         suggestions.map((suggestion, index) => (
           <Button onClick={() => onSelect(index)} key={index}>{suggestion.name}</Button>
