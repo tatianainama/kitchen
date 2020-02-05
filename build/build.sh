170fca081d3421d151e966c7e89e289e012f6ea4
david <davidventura27@gmail.com>
2020-02-05 23:49:04 +0100

    update build

--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker build -f Dockerfile.base  . -t ktchn
-docker build -f Dockerfile.build  .  -t ktchn_build
+docker build -f Dockerfile.build  --no-cache .  -t ktchn_build
 docker create --name build_output -it ktchn_build
-docker cp build_output:/kitchn/ktchn/dist/bundle.js output/
-docker cp build_output:/kitchn/recipes/build/ output/
+docker cp build_output:/kitchn/ktchn/dist/bundle.js backend_output/
+docker cp build_output:/kitchn/recipes/build/ frontend_output/
 docker rm -f build_output
