373aa4db975cf800bef458563d3b75ddcba6d8bb
david <davidventura27@gmail.com>
2020-02-26 22:42:15 +0100

    Update build rules

--- build/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker build -f Dockerfile.base  . -t ktchn
-docker build -f Dockerfile.build  --no-cache .  -t ktchn_build
-docker create --name build_output -it ktchn_build
-docker cp build_output:/kitchn/ktchn/dist/bundle.js backend_output/
-docker cp build_output:/kitchn/recipes/build/ frontend_output/
-docker rm -f build_output
