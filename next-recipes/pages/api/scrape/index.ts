50004318f779b4241f0e451f488cf36a448b207e
Tatiana Inama <tatii.oxo@gmail.com>
2022-02-12 20:19:13 +0100

    Improve error handling

--- next-recipes/pages/api/scrape/index.ts
+++ next-recipes/pages/api/scrape/index.ts
@@ -1,12 +1,23 @@
-/* eslint-disable id-length */
 import { NextApiHandler } from 'next';
 import scrape from '@/utils/api/scrape';
 
+enum HttpStatus {
+  BadRequest = 400,
+  ServerError = 500,
+}
 
 const handler: NextApiHandler = async (req, res) => {
   const { url } = req.body;
-  const result = await scrape(url);
-  res.json(result);
+  if (!url) {
+    res.status(HttpStatus.BadRequest).json({ error: 'Invalid param url, cannot be empty' });
+    return;
+  }
+  try {
+    const result = await scrape(url);
+    res.json(result);
+  } catch (e) {
+    res.status(HttpStatus.ServerError).json({ error: `Error while scraping url ${url}: ${e}` });
+  }
 };
 
 export default handler;
