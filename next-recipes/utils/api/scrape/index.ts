8ee199ff3b636702a81f724c64e9e45ce290c064
Tatiana Inama <tatii.oxo@gmail.com>
2022-04-02 13:16:42 +0200

    Add user agent to scrape headers

--- next-recipes/utils/api/scrape/index.ts
+++ next-recipes/utils/api/scrape/index.ts
@@ -5,7 +5,11 @@ import getScrapingSource from '@/utils/api/scrape/sources';
 import { parseFromJsonLd } from './parser/recipe';
 
 const scrape = async (url: string): Promise<RecipeTypes.ScrapedRecipe> => {
-  const response = await axios.get(url);
+  const response = await axios.get(url, {
+    headers: {
+      'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36'
+    }
+  });
   const $ = cheerio.load(response.data);
 
   const jsonLdData = parseFromJsonLd($);
