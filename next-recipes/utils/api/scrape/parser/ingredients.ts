95c661f46b8552730e3a6df14de42fe1d5a85612
Tatiana Inama <tatii.oxo@gmail.com>
2022-04-09 17:54:07 +0200

    Parse units when scraping recipes

--- next-recipes/utils/api/scrape/parser/ingredients.ts
+++ next-recipes/utils/api/scrape/parser/ingredients.ts
@@ -1,14 +1,14 @@
+import { UnitName } from '@prisma/client';
 import { parse } from 'recipe-ingredient-parser-v2';
 
 export type ParsedIngredient = {
   quantity: string;
-  unit: string;
+  unit: UnitName | null;
   ingredient: string;
   note: string;
   original: string;
 };
 
-// eslint-disable-next-line no-extra-parens
 const sanitizeIngredient = (ingredient: string): string =>
   ingredient.startsWith('of') ? ingredient.replace('of ', '') : ingredient;
 
@@ -31,11 +31,11 @@ const getNotes = (ingredient: string): { ingredient: string; note: string } => {
 
 const parseIngredient = (ingredientString: string): ParsedIngredient => {
   try {
-    const parsedIngredient = parse(ingredientString);
+    const parsedIngredient = parse(ingredientString.toLowerCase());
     const { ingredient, note } = getNotes(parsedIngredient.ingredient);
     return {
       quantity: parsedIngredient.quantity,
-      unit: parsedIngredient.unit,
+      unit: parseUnit(parsedIngredient.unit),
       ingredient: sanitizeIngredient(ingredient),
       note,
       original: ingredientString
@@ -44,12 +44,43 @@ const parseIngredient = (ingredientString: string): ParsedIngredient => {
     const { ingredient, note } = getNotes(ingredientString);
     return {
       quantity: null,
-      unit: null,
+      unit: UnitName.UNIT,
       ingredient: sanitizeIngredient(ingredient),
       note,
       original: ingredientString
     };
   }
+};
+
+const possibilities = {
+  [UnitName.CUP]: ['CUPS', 'CPS'],
+  [UnitName.CLOVES]: ['CLOVE'],
+  [UnitName.G]: ['GR', 'GRAM', 'GRAMS', 'GRS'],
+  [UnitName.GAL]: ['GALLON', 'GALS', 'GALLONS'],
+  [UnitName.L]: ['LITRE', 'LITER', 'LITERS'],
+  [UnitName.ML]: ['MILS', 'MILILITER', 'MILLILITERS', 'MILLILITER'],
+  [UnitName.LB]: ['POUND', 'POUNDS'],
+  [UnitName.OZ]: ['OUNCE', 'OUNCES'],
+  [UnitName.PINCH]: ['PINCHES'],
+  [UnitName.TBSP]: ['TABLESPOON', 'TBP', 'TP'],
+  [UnitName.TSP]: ['TEASPOON', 'TS']
+};
+
+export const parseUnit = (unitInput?: string): UnitName | null => {
+  if (!unitInput) {
+    return null;
+  }
+  const test = unitInput.toUpperCase().replaceAll(' ', '_');
+  const values = Object.values(UnitName);
+  if (values.some((unit) => unit === test)) {
+    return UnitName[test];
+  }
+  for (const [unit, values] of Object.entries(possibilities)) {
+    if (values.includes(test)) {
+      return unit as UnitName;
+    }
+  }
+  return null;
 };
 
 export default parseIngredient;
